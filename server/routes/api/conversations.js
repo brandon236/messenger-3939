ff1b960f0175a251550adb3621a5f8518e67600d
brandon236 <BrandonMacleod21@gmail.com>
2021-08-28 01:33:36 -0700

    Implemented unread messages calculation on the server

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -18,7 +18,7 @@ router.get("/", async (req, res, next) => {
           user2Id: userId,
         },
       },
-      attributes: ["id"],
+      attributes: ["id", "dateLastAccessed"],
       order: [[Message, "createdAt", "DESC"]],
       include: [
         { model: Message, order: ["createdAt", "DESC"] },
@@ -46,11 +46,38 @@ router.get("/", async (req, res, next) => {
         },
       ],
     });
+    // const date = new Date(Date.now()).toISOString();
+    // const values = {
+    //   "dateLastAccessed": date
+    // }
+    // const selector = {
+    //   where : {}
+    // }
+    // const tempcon = await Conversation.update(values, selector);
+    // console.log("values");
+    // console.log(values);
 
     for (let i = 0; i < conversations.length; i++) {
       const convo = conversations[i];
       const convoJSON = convo.toJSON();
-
+      if (convoJSON.dateLastAccessed !== null) {
+        let unreadCount = 0;
+        for (let j = convo.messages.length-1; j >= 0 ; j--) {
+          console.log(convo.messages[j].createdAt);
+          if (Date.parse(convo.messages[j].createdAt) > Date.parse(convoJSON.dateLastAccessed)) {
+            unreadCount++;
+          } else {
+            break;
+          }
+        }
+        convoJSON.unreadMessages = unreadCount;
+      } else {
+        convoJSON.unreadMessages = 0;
+        const newDate = new Date(Date.now());
+        convoJSON.dateLastAccessed = newDate.toISOString();
+      }
+      // console.log("messages")
+      // console.log(convo.messages);
       // set a property "otherUser" so that frontend will have easier access
       if (convoJSON.user1) {
         convoJSON.otherUser = convoJSON.user1;
@@ -70,7 +97,11 @@ router.get("/", async (req, res, next) => {
       const reversedMessages = convoJSON.messages.slice(0).reverse();
       convoJSON.messages = reversedMessages;
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[convoJSON.messages.length-1].text;
+      console.log("messages");
+      console.log(convoJSON);
+      if (convoJSON.messages.length !== 0) {
+        convoJSON.latestMessageText = convoJSON.messages[convoJSON.messages.length-1].text;
+      }
       conversations[i] = convoJSON;
     }
 
@@ -79,5 +110,37 @@ router.get("/", async (req, res, next) => {
     next(error);
   }
 });
+
+
+router.post("/", async (req, res, next) => {
+  // console.log("postFunction");
+  // console.log(req.body);
+  const {id, dateLastAccessed} = req.body;
+  console.log(id);
+  console.log(typeof dateLastAccessed);
+  try {
+    // console.log("conversation");
+    // console.log(Conversation.create());
+    // const testConvo = await Conversation.create({
+    //   user1Id: 3,
+    //   user2Id: 9,
+    // });
+    // console.log("testconvo");
+    // console.log(testConvo);
+    const tempcon = await Conversation.update({ "dateLastAccessed": dateLastAccessed, }, {
+      where: {
+        id: id,
+        returning: true,
+        plain: true
+      }
+    });
+    console.log("tempcon2")
+    console.log(tempcon);
+    res.json({ tempcon });
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 module.exports = router;
