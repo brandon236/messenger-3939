b467b7bcb1ba4cc8b2d5f1ca050f4da2f6d4a570
brandon236 <BrandonMacleod21@gmail.com>
2021-09-01 18:42:26 -0700

    Modified seed to include dateLastAccessed

--- server/db/seed.js
+++ server/db/seed.js
@@ -26,6 +26,7 @@ async function seed() {
   const santaigoConvo = await Conversation.create({
     user1Id: thomas.id,
     user2Id: santiago.id,
+    dateLastAccessed: new Date(Date.now()).toISOString(),
   });
 
   await Message.create({
@@ -54,6 +55,7 @@ async function seed() {
   const chiumboConvo = await Conversation.create({
     user1Id: chiumbo.id,
     user2Id: thomas.id,
+    dateLastAccessed: new Date(Date.now()).toISOString(),
   });
   await Message.create({
     conversationId: chiumboConvo.id,
@@ -71,6 +73,7 @@ async function seed() {
   const hualingConvo = await Conversation.create({
     user2Id: hualing.id,
     user1Id: thomas.id,
+    dateLastAccessed: new Date(Date.now()).toISOString(),
   });
 
   for (let i = 0; i < 11; i++) {
