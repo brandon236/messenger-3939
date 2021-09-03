9872ff71c616ee70092eafbe6a8246627db336b4
brandon236 <BrandonMacleod21@gmail.com>
2021-09-02 17:43:46 -0700

    Modified seed to include readStatus

--- server/db/seed.js
+++ server/db/seed.js
@@ -33,16 +33,19 @@ async function seed() {
     conversationId: santaigoConvo.id,
     senderId: santiago.id,
     text: "Where are you from?",
+    readStatus: true,
   });
   await Message.create({
     conversationId: santaigoConvo.id,
     senderId: thomas.id,
     text: "I'm from New York",
+    readStatus: true,
   });
   await Message.create({
     conversationId: santaigoConvo.id,
     senderId: santiago.id,
     text: "Share photo of your city, please",
+    readStatus: true,
   });
 
   const chiumbo = await User.create({
@@ -61,6 +64,7 @@ async function seed() {
     conversationId: chiumboConvo.id,
     senderId: chiumbo.id,
     text: "Sure! What time?",
+    readStatus: true,
   });
 
   const hualing = await User.create({
@@ -81,6 +85,7 @@ async function seed() {
       conversationId: hualingConvo.id,
       senderId: hualing.id,
       text: "a test message",
+      readStatus: true,
     });
   }
 
@@ -88,6 +93,7 @@ async function seed() {
     conversationId: hualingConvo.id,
     senderId: hualing.id,
     text: "😂 😂 😂",
+    readStatus: true,
   });
 
   const otherUsers = await Promise.all([
